8087530e45ef0ef18ceec9bb4093cb4539cc0370
thyoondev <th.yoon.dev@gmail.com>
2021-03-27 16:44:08 +0900

    lombok 세팅 및 사용해보기

--- blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -16,8 +16,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 
-	//인터넷 브라우저 요청은 무조건 get 요청밖에 할 수가 없다.
-	//http://localhost:8080/http/get(select)
+	private static final String TAG = "HttpConstrollerTest : ";
+	
+	/**
+	 * 롬복 이용
+	 * @return
+	 */
+//	@GetMapping("/http/lombok")
+//	public String lombokTest() {
+//		Member m = new Member(1, "ssar", "1234", "email");
+//		System.out.println(TAG + " getter : " + m.getUsername());
+//		m.setUsername("홍길동");
+//		System.out.println(TAG + " setter : " + m.getUsername());
+//		return "lombok test 완료";
+//	}
+	
+	/**
+	 * @Builder 어노테이션을 이용한 객체 생성
+	 * @return
+	 */
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build(); //순서 상관없음
+		System.out.println(TAG + " getter : " + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG + " setter : " + m.getUsername());
+		return "lombok test 완료";
+	}
+	
+
+	/**
+	 *  인터넷 브라우저 요청은 무조건 get 요청밖에 할 수가 없다.
+	 * http://localhost:8080/http/get(select)
+	 * @param m
+	 * @return
+	 */
 	
 //	@GetMapping("/http/get")
 //	public String getTest(@RequestParam int id, @RequestParam String username) {
@@ -29,7 +62,11 @@ public class HttpControllerTest {
 		return "get 요청 : " + m.getId() + ", " + "username : " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	//http://localhost:8080/http/post(insert)
+	/**
+	 * http://localhost:8080/http/post(insert)
+	 * @param m
+	 * @return
+	 */
 	
 //	@PostMapping("/http/post")
 //	public String postTest(Member m) {
@@ -46,13 +83,20 @@ public class HttpControllerTest {
 		return "post 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	//http://localhost:8080/http/put(update)
+	/**
+	 * http://localhost:8080/http/put(update)
+	 * @param m
+	 * @return
+	 */
 	@PutMapping("/http/put")
 	public String putTest(@RequestBody Member m) {
 		return "put 요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword() + ", " + m.getEmail();
 	}
 	
-	//http://localhost:8080/http/delete(delete)
+	/**
+	 * http://localhost:8080/http/delete(delete)
+	 * @return
+	 */
 	@DeleteMapping("/http/delete")
 	public String deleteTest() {
 		return "delete 요청";
--- blog/src/main/java/com/cos/blog/test/Member.java
+++ blog/src/main/java/com/cos/blog/test/Member.java
@@ -1,44 +1,53 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+//lombok getter 와 setter 생성을 간단하게!
+//@Getter
+//@Setter
+//@Data
+//@AllArgsConstructor
+//@RequiredArgsConstructor //final 붙은 애들의 Constructor를 생성해줌
+
+/**
+ * 어노테이션으로 하는 방식
+ */
+//@Data
+//@AllArgsConstructor //전체생성자
+//@NoArgsConstructor //빈생성자
+//public class Member {
+//
+//	private int id; //final은 불면성을 위해 사용
+//	private String username;
+//	private String password;
+//	private String email;
+//	
+	
+/**
+ * constructor를 오버로딩 없이 만드는 방법
+ */
+@Data
+@NoArgsConstructor //빈생성자
 public class Member {
 
-	private int id;
+	private int id; //final은 불면성을 위해 사용
 	private String username;
 	private String password;
 	private String email;
 	
-	
+	@Builder
 	public Member(int id, String username, String password, String email) {
+		super();
 		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
 	
 }
