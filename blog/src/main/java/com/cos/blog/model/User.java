f43576e49cc0abc8e8d6d81367c68d67b9d86a3e
thyoondev <th.yoon.dev@gmail.com>
2021-03-27 18:54:55 +0900

    JPA로 User Table 생성

---
+++ blog/src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,37 @@
+package com.cos.blog.model;
+
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+//ORM -> JAVA(다른언어) Object -> 테이블로 맵핑
+@Entity //User 클래스가 자동으로 MySQL에 테이블이 생성된다.
+public class User {
+ 
+	@Id //Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.(오라클: 시퀀스, MySQL : auto_increment)
+	private int id;
+	
+	@Column(nullable = false, length = 30)
+	private String username; //아이디
+	
+	@Column(nullable = false, length = 100) //123456 => hash(비밀번호 암호화)
+	private String password; 
+	
+	@Column(nullable = false, length = 50)
+	private String email;
+	
+	@ColumnDefault("'user'")
+	private String role; //Enum을 쓰는게 좋다.(도메인 설정 가능 = [범위 ex) 남, 녀]) //Admin, user, manager
+	
+	@CreationTimestamp //시간이 자동 입력
+	private Timestamp createDate;
+}
