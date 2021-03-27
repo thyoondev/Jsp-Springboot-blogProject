274ea1c4d1225e68084a8862898b7acdb426bc01
thyoondev <th.yoon.dev@gmail.com>
2021-03-27 20:34:38 +0900

    JPA의 DB연관관계의 주인 설정

--- blog/src/main/java/com/cos/blog/model/Board.java
+++ blog/src/main/java/com/cos/blog/model/Board.java
@@ -1,15 +1,18 @@
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,10 +42,16 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; //조회수
 
-	@ManyToOne //Board = Many, User = One //한명의 유저는 여러개의 게시글을 작성할 수 있다.
+	@ManyToOne(fetch = FetchType.EAGER) //Board = Many, User = One //한명의 유저는 여러개의 게시글을 작성할 수 있다.
 	@JoinColumn(name = "userId")
 	private User user; //DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
 	//private int userId;
+	
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER) //mappedBy가 적혀있으면 연관관계의 주인이 아니다(난 FK가 아니에요) DB에 칼럼을 만들지 마세요.
+	//FetchType.EAGER 화면 보여줄 때 바로 가져옴 ex)댓글창 바로 보일 때
+	//FetchType.LAZY 화면 보여줄 때 바로 가져오지 않고 필요할 때 가져옴 ex)댓글창 바로 안보이고 더보기.. 같은 버튼 눌렀을 때
+	//@JoinColumn(name = "replyId")은 필요없음
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
