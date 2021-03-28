63f863fdf6cd2312766cee0e480c42adf33efbe8
thyoondev <th.yoon.dev@gmail.com>
2021-03-28 20:23:50 +0900

    update 테스트

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -29,6 +33,30 @@ public class DummyControllerTest {
 	@Autowired //의존성 주입 DI
 	private UserRepository userRepository;
 	
+	//save함수는 id를 전달하지 않으면 insert를 해주고
+	//save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를해주고
+	//save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해요.
+	//http://localhost:8000/blog/dummy/user/1
+	//email, password 수정
+	
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //json 데이터를 요청 => Java Object(MassageConverter의 Jackson 라이브러리가 변환해서 받아줌)
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+//		userRepository.save(user); 
+		
+		//더티 체킹
+		return null;
+	}
 	
 	//http://localhost:8000/blog/dummy/user
 	@GetMapping("/dummy/users")
