590f638168bb79f261dc8bf02aa1d1eed6242be9
thyoondev <th.yoon.dev@gmail.com>
2021-03-28 18:03:19 +0900

    select 테스트

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.cos.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +12,45 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
+//html파일이 아니라 data를 리턴해주는 controller
 @RestController
 public class DummyControllerTest {
 
 	@Autowired //의존성 주입 DI
 	private UserRepository userRepository;
+	
+	//{id} 주소로 파마메터를 전달 받을 수 있음
+	//http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		//user/4를 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 될 것 아냐?
+		//그럼 return nill이 되잖아.. 그럼 프로그램에 문제가 있지 않겠니?
+		//Optional로 너의 User 객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return해!!
+		
+		
+		//람다식
+//		User user = userRepository.findById(id).orElseThrow(()->{
+//				return new IllegalArgumentException("해당 유저는 없습니다. ID :" + id);
+//		});
+
+	
+		//일반
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다. ID :" + id);
+			}
+		});
+		
+		//요청 웹 브라우저
+		//user 객체 = 자바 오브젝트
+		//변환 (웹브라우저가 이해할 수 있는 데이터) -> json (Gson 라이브러리)
+		//스프링부트 = MessageConverter가 응답시에 자동 작동
+		//만약 자바 오브젝트를 리턴하게 되면 MassageConverter가 Jackson 라이브러리를 호출해서 
+		//user 오브젝트를 json으로 변환해서 브라우저에 던져줍니다.
+		return user;
+	}
+	
 	
 	//http://localhost:8000/blog/dummy/join(요청)
 	//http의 body에 username, password, email 데이터를 가지고 요청(요청)
