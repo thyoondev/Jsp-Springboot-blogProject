aaace32b0290aeea28eff78ce616db56d087077e
thyoo <thyoo@DESKTOP-MLEB4HA>
2021-04-02 11:40:46 +0900

    전통적인 방식의 로그인 구현

--- blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +19,27 @@ public class UserApiController {
 	@Autowired
 	private UserService UserService;
 	
+	@Autowired
+	private HttpSession session;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController: : save호출됌");
 		// 실제로 DB에 insert를 하고 아래에서 return이 되면 돼요.
 		user.setRole(RoleType.USER);
-		int result = UserService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), result);
+		UserService.회원가입(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바오브젝트를 json으로 변환해서 리턴(jackson)
+	}
+	
+	//전통적인 방식의 로그인
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user){
+		System.out.println("UserApiController: : login호출됌");
+		User principal = UserService.로그인(user); //principal 접근주체
+		
+		if(principal != null) {
+			session.setAttribute("principal", principal);
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
 	}
 }
--- blog/src/main/java/com/cos/blog/repository/UserRepository.java
+++ blog/src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cos.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cos.blog.model.User;
 
@@ -8,5 +9,12 @@ import com.cos.blog.model.User;
 //자동으로 Bean으로 등록이 된다.
 //@Repository 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer>{
-
+	//JPA Naming전략 (이름으로 쿼리 자동생성)
+	//select * form where username = ?1 and password = ?2;
+	//방법1
+	User findByUsernameAndPassword(String username, String password);
+	
+	//방법2(native query)
+//	@Query(value = "select * form where username = ?1 and password = ?2", nativeQuery = true)
+//	User login(String username, String password);
 }
